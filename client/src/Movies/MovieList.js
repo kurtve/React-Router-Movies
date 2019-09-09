c8a1d4a6eaa952adb078ad089f013d241981a739
Kurt Van Etten <kurt@vanetten.us>
2019-09-08 21:13:59 -0400

    Move movie card display logic from Movie and MovieList into MovieCard component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import MovieCard from './MovieCard';
 
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
-      
+
       axios
         .get('http://localhost:5000/api/movies')
         .then(response => {
@@ -24,7 +25,7 @@ const MovieList = props => {
       {movies.map(movie => {
         return (
           <Link to={`/movies/${movie.id}`} key={movie.id} >
-            <MovieDetails movie={movie} />
+            <MovieCard movie={movie} />
           </Link>
         );
       })}
@@ -32,27 +33,5 @@ const MovieList = props => {
   );
 }
 
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
 
 export default MovieList;
